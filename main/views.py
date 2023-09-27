ab4e00eb46501b943fd3a0525e5de5e3b776dd3f
edbert2397 <edberthalim29@gmail.com>
2023-09-27 14:07:46 +0700

    tutorial 4 kelar

--- main/views.py
+++ main/views.py
@@ -17,6 +17,7 @@ from django.urls import reverse
 
 @login_required(login_url='/login')
 def show_main(request):
+    print(request)
     products = Product.objects.filter(user=request.user)
 
     context = {
@@ -28,6 +29,29 @@ def show_main(request):
     }
 
     return render(request, "main.html", context)
+
+def delete_product(request, id):
+    # Get data berdasarkan ID
+    product = Product.objects.get(pk = id)
+    # Hapus data
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+def edit_product(request, id):
+    # Get product berdasarkan ID
+    product = Product.objects.get(pk = id)
+
+    # Set product sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
 
 def create_product(request):
     form = ProductForm(request.POST or None)
